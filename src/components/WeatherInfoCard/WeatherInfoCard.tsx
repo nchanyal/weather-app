a212a5ea34ce5cedc8b5f1bab8d49a39487b4ef9
nathnael <134805961+nchanyal@users.noreply.github.com>
2025-12-06 12:07:04 -0500

    style: match design specs for WeatherInfoCard

--- src/components/WeatherInfoCard/WeatherInfoCard.tsx
+++ src/components/WeatherInfoCard/WeatherInfoCard.tsx
@@ -1,13 +1,25 @@
+import Image from "next/image";
+import WeatherInfoBgLg from "../../../public/bg-today-large.svg";
+
+import SunSVG from "../../../public/weather-icons/Clear-sunny.svg";
+
 export default function WeatherInfoCard() {
   return (
-    <div className="col-span-4 bg-linear-to-br from-blue-500 to-blue-700 radius-20 flex items-center justify-between">
-      <div>
-        <div>Location</div>
-        <div>Date</div>
-      </div>
-      <div>
-        <div>Weather Icon</div>
-        <div>Temperature</div>
+    <div className="relative col-span-4 radius-20 overflow-hidden">
+      <Image
+        src={WeatherInfoBgLg}
+        alt="Dark, cloudy night background"
+        className="absolute inset-0 w-full h-full object-cover"
+      />
+      <div className="relative z-10 h-full flex items-center justify-between py-20 px-6">
+        <div className="flex flex-col gap-3">
+          <p className="text-preset-4">Berlin, Germany</p>
+          <p className="text-preset-6 opacity-80">Tuesday, Aug 5, 2025</p>
+        </div>
+        <div className="flex gap-5 items-center">
+          <Image src={SunSVG} alt="Sun" width={120} height={120} />
+          <p className="text-preset-1">20°</p>
+        </div>
       </div>
     </div>
   );

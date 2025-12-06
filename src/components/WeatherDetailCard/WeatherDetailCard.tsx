635ad8495d8345b48570b9ed19969dfc44f5cd40
nathnael <134805961+nchanyal@users.noreply.github.com>
2025-12-06 12:34:52 -0500

    style: match design specs for WeatherDetailCard

--- src/components/WeatherDetailCard/WeatherDetailCard.tsx
+++ src/components/WeatherDetailCard/WeatherDetailCard.tsx
@@ -6,9 +6,11 @@ export default function WeatherDetailCard({
   value: string;
 }) {
   return (
-    <div className="bg-neutral-800 radius-12 border border-neutral-600 flex flex-col justify-between">
-      <div>{title}</div>
-      <div>{value}</div>
+    <div className="bg-neutral-800 radius-12 border border-neutral-600 p-5">
+      <div className="h-full flex flex-col justify-between">
+        <p className="text-preset-6 text-neutral-200">{title}</p>
+        <p className="text-preset-3">{value}</p>
+      </div>
     </div>
   );
 }
